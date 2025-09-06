5f74463fa548e7ed1bf41f68a0fd0234147f9e53
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-06 10:11:31 -0500

    feat(greet-developer): add request and response resources.

---
+++ Acme.Hello.Platform/Generic/Interfaces/REST/Resources/GreetDeveloperResponse.cs
@@ -0,0 +1,17 @@
+namespace Acme.Hello.Platform.Generic.Interfaces.REST.Resources;
+
+/// <summary>
+/// Represents a response to a greeting request. 
+/// </summary>
+/// <param name="Id">The ID of the developer, which can be null.</param>
+/// <param name="FirstName">The first name of the developer, which can be null.</param>
+/// <param name="LastName">The last name of the developer, which can be null.</param>
+/// <param name="Message">The greeting message to be returned to the caller.</param>
+public record GreetDeveloperResponse(Guid? Id, string? FirstName, string? LastName, string Message)
+{
+    /// <summary>
+    /// Creates a new GreetDeveloperResponse with a message. 
+    /// </summary>
+    /// <param name="message">The greeting message to be returned to the caller.</param>
+    public GreetDeveloperResponse(string message) : this(null, null, null, message) { }
+}
