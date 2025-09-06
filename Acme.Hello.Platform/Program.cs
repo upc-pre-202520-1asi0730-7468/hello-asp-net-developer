15690d01d1e5100b1f9f8a7809c25021e5bf616e
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-06 10:44:37 -0500

    feat(greet-developer): add endpoints behavior.

--- Acme.Hello.Platform/Program.cs
+++ Acme.Hello.Platform/Program.cs
@@ -1,15 +1,45 @@
+using Acme.Hello.Platform.Generic.Domain.Model.Entities;
+using Acme.Hello.Platform.Generic.Interfaces.REST.Assemblers;
+using Acme.Hello.Platform.Generic.Interfaces.REST.Resources;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
-app.UseHttpsRedirection();
+app.MapGet("/greetings", (string? firstName, string? lastName) =>
+    {
+        // Verify if the request is valid.
+        var developer = !string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName)
+            ? new Developer(firstName, lastName)
+            : null;
+        // Assemble the response.
+        var response = GreetDeveloperResponseAssembler.ToResponseFromEntity(developer);
+        // Return the response.
+        return Results.Ok(response);
+    })
+    .WithName("GetGreeting")
+    .WithOpenApi();
 
-app.MapGet("/weatherforecast", () => { })
-    .WithName("GetWeatherForecast");
+app.MapPost("/greetings", (GreetDeveloperRequest request) =>
+    {
+        // Assemble the entity from the request.
+        var developer = DeveloperAssembler.ToEntityFromRequest(request);
+        // Assemble the response.
+        var response = GreetDeveloperResponseAssembler.ToResponseFromEntity(developer);
+        // Return the response.
+        return Results.Created("/greetings", response);
+    })
+    .WithName("CreateGreeting")
+    .WithOpenApi();
 
 app.Run();
